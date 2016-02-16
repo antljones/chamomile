77ff29329321d1d203aea8813e17b46b157ca09c
Katherine Elizabeth Lightsey <KELightsey@gmail.com>
2016-02-16 10:14:46 -0800

    Updated naming

--- tool/lines_of_code.sql
+++ tool/lines_of_code.sql
@@ -1,17 +1,17 @@
-SELECT o.type_desc                                                        AS ROUTINE_TYPE
-       , QUOTENAME(s.[name]) + '.'
-         + QUOTENAME(o.[name])                                            AS [OBJECT_NAME]
-       , ( LEN(m.definition) - LEN(REPLACE(m.definition, CHAR(10), '')) ) AS LINES_OF_CODE
-FROM   sys.sql_modules AS m
-       INNER JOIN sys.objects AS o
-               ON m.[object_id] = o.[OBJECT_ID]
-       INNER JOIN sys.schemas AS s
-               ON s.[schema_id] = o.[schema_id];
+SELECT [objects].[type_desc]                                                                          AS [type]
+       , QUOTENAME([schemas].[name]) + '.'
+         + QUOTENAME([objects].[name])                                                                AS [object_name]
+       , ( LEN([sql_modules].[definition]) - LEN(REPLACE([sql_modules].[definition], CHAR(10), '')) ) AS [lines_of_code]
+FROM   [sys].[sql_modules] AS [sql_modules]
+       INNER JOIN [sys].[objects] AS [objects]
+               ON [sql_modules].[object_id] = [objects].[object_id]
+       INNER JOIN [sys].[schemas] AS [schemas]
+               ON [schemas].[schema_id] = [objects].[schema_id];
 
-SELECT sum (LEN(m.definition) - LEN(REPLACE(m.definition, CHAR(10), ''))) AS [total_lines_of_code]
-       , count(*)                                                         AS [total_objects]
-FROM   sys.sql_modules AS m
-       INNER JOIN sys.objects AS o
-               ON m.[object_id] = o.[OBJECT_ID]
-       INNER JOIN sys.schemas AS s
-               ON s.[schema_id] = o.[schema_id]; 
+SELECT sum (LEN([sql_modules].[definition]) - LEN(REPLACE([sql_modules].[definition], CHAR(10), ''))) AS [total_lines_of_code]
+       , count(*)                                                                                     AS [total_objects]
+FROM   [sys].[sql_modules] AS [sql_modules]
+       INNER JOIN [sys].[objects] AS [objects]
+               ON [sql_modules].[object_id] = [objects].[object_id]
+       INNER JOIN [sys].[schemas] AS [schemas]
+               ON [schemas].[schema_id] = [objects].[schema_id]; 
