f2e6d1096e76775ea092849a936419ee8abaab0d
Katherine Elizabeth Lightsey <KELightsey@gmail.com>
2016-05-31 10:39:51 -0700

    Update get_table_size.sql

--- tool/get_table_size.sql
+++ tool/get_table_size.sql
@@ -25,12 +25,12 @@
 	-- references
 	---------------------------------------------
 */
-select [schemas].[name]                                                                   as [schema]
-       , [tables].[name]                                                                  as [table]
-       , [partitions].[rows]                                                              as [row_count]
-       , sum([allocation_units].total_pages) * 8                                          as [total_space_kb]
-       , sum([allocation_units].used_pages) * 8                                           as [used_space_kb]
-       , ( sum([allocation_units].total_pages) - sum([allocation_units].used_pages) ) * 8 as [unused_space_kb]
+select [schemas].[name]                                                                   	as [schema]
+       , [tables].[name]                                                                  	as [table]
+       , [partitions].[rows]                                                              	as [row_count]
+       , sum([allocation_units].[total_pages]) * 8                                          	as [total_space_kb]
+       , sum([allocation_units].[used_pages]) * 8                                           	as [used_space_kb]
+       , ( sum([allocation_units].[total_pages]) - sum([allocation_units].[used_pages]) ) * 8 	as [unused_space_kb]
 from   [sys].[tables] as [tables]
        inner join [sys].[schemas] as [schemas]
                on [schemas].[schema_id] = [tables].[schema_id]
@@ -40,11 +40,10 @@ from   [sys].[tables] as [tables]
                on [indexes].[object_id] = [partitions].[object_id]
                   and [indexes].[index_id] = [partitions].[index_id]
        inner join [sys].[allocation_units] as [allocation_units]
-               on [partitions].[partition_id] = [allocation_units].container_id
+               on [partitions].[partition_id] = [allocation_units].[container_id]
 where  [tables].[name] not like 'dt%'
        and [tables].is_ms_shipped = 0
        and [indexes].[object_id] > 255
-       and object_schema_name([tables].[object_id]) = N'dbo'
 group  by [schemas].[name]
           , [tables].[name]
           , [partitions].[rows]
