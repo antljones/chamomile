650129b8038850a928efe1016ea446934d7c11b9
Katherine Elizabeth Lightsey <KELightsey@gmail.com>
2017-11-17 09:06:20 -0700

    Update get_statistics_information_for_table.sql

--- tool/get_statistics_information_for_table.sql
+++ tool/get_statistics_information_for_table.sql
@@ -1,3 +1,21 @@
+SELECT quotename(db_name()) + N'.'
+       + quotename([schemas].[name]) + N'.'
+       + quotename([objects].[name])                     AS [object]
+       , [objects].[type_desc]                           AS [object_type]
+       , [indexes].[name]                                AS [index]
+       , [indexes].[type_desc]                           AS [index_type]
+       , [dm_db_stats_properties].[last_updated]         AS [last_updated]
+       , [dm_db_stats_properties].[rows]				 AS [rows]
+       , [dm_db_stats_properties].[modification_counter] AS [modification_counter]
+       , *
+FROM   [sys].[indexes] AS [indexes]
+       JOIN [sys].[objects] AS [objects]
+         ON [objects].[object_id] = [indexes].[object_id]
+       JOIN [sys].[schemas] AS [schemas]
+         ON [schemas].[schema_id] = [objects].[schema_id]
+       CROSS apply [sys].[dm_db_stats_properties]([objects].[object_id], [indexes].[index_id]) AS [dm_db_stats_properties]
+ORDER BY [dm_db_stats_properties].[last_updated] ASC;
+
 --
 -- http://www.sommarskog.se/query-plan-mysteries.html#statinfo
 /*
