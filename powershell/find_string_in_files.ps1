36c3f8f29894c0b70ae081c96fa936be838e0038
Katherine Elizabeth Lightsey <KELightsey@gmail.com>
2018-06-11 14:09:19 -0700

    Update find_string_in_files.ps1

--- powershell/find_string_in_files.ps1
+++ powershell/find_string_in_files.ps1
@@ -5,3 +5,7 @@ Get-ChildItem -Recurse | Select-String -Pattern "find_me" | group path | select 
 -- 
 -- get context, 2 lines before and 3 after
 Get-ChildItem -Recurse | Select-String -Pattern "find_me" -Context 2, 3
+
+-- complex https://ardalis.com/find-string-in-files-with-given-extension-using-powershell
+$file_list = get-childitem -recurse | where {$_.extension -eq ".<extension>"}
+select-string <find_me> $file_list | format-table path | out-file result_01.txt
