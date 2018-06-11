5cfbfefcc9c6e3f5cdc18e44c6bc5f9d7d05b5ee
Katherine Elizabeth Lightsey <KELightsey@gmail.com>
2018-06-11 13:15:00 -0700

    Update find_string_in_files.ps1

--- powershell/find_string_in_files.ps1
+++ powershell/find_string_in_files.ps1
@@ -1,3 +1,7 @@
 Get-ChildItem -Recurse | Select-String "advo" | Select-Object Filename | Out-File contain_advo.txt
 
 Get-ChildItem -Recurse | Select-String -Pattern "find_me" | group path | select name
+
+-- 
+-- get context, 2 lines before and 3 after
+Get-ChildItem -Recurse | Select-String -Pattern "find_me" -Context 2, 3
