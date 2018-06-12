bfed0a8e251084cb209c8b42c330772f11d358d2
Katherine Elizabeth Lightsey <KELightsey@gmail.com>
2018-06-12 15:54:00 -0700

    Update and rename get_raw_report.sql to get_ssrs_information.sql

--- ssrs/get_raw_report.sql
+++
@@ -1,3 +0,0 @@
- -- http://mscrmuk.blogspot.com/2009/05/reading-rdl-definitions-directly-from.html 
- SELECT Name, convert (varchar(max), convert (varbinary(max),[Content])) AS ReportRDL
- FROM [dbo].[Catalog] where TYPE =2
